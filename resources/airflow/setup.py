c496b7f8a3190664af9eeefc369b7c91c0ce1e08
AGutierrezTorres <81990075+AGutierrezTorres@users.noreply.github.com>
2022-12-25 16:58:08 +0100

    Update setup.py

--- resources/airflow/setup.py
+++ resources/airflow/setup.py
@@ -8,7 +8,12 @@ import iso8601
 
 PROJECT_HOME = os.getenv("PROJECT_HOME")
 
-
+# En estas propiedades iniciales se define el numero de intentos que debe 
+# realizar el DAG antes de que no se ejecute y muera con la propiedad retries
+#(3 en este caso).
+# Se indica desde la fecha por la que debe comenzar("2016-12-01").
+# Y,finalmente,con retry_delay se define cada cuanto tiempo se realizan los 
+#reintentos(5min en este caso).
 default_args = {
   'owner': 'airflow',
   'depends_on_past': False,
@@ -17,6 +22,10 @@ default_args = {
   'retry_delay': timedelta(minutes=5),
 }
 
+# En esta parte se configura el DAG con las propiedades definidas anteriormente
+# y sele da un nombre. También se le asigna un schedule_interval para definir 
+# cada cuanto tiempo se debe realizar este DAG. Otros posibles valores del schedule_interval
+#son: @once @hourly @daily
 training_dag = DAG(
   'agile_data_science_batch_prediction_model_training',
   default_args=default_args,
@@ -51,11 +60,17 @@ extract_features_operator = BashOperator(
 
 """
 
+# Aqui se define el BashOperator con el que se determina que tarea se va a ejecutar 
+# en el DAG. Dentro se definen la dirección del master de Spark("local[8]"), 
+#el archivo de entrenamiento ("resources/train_spark_mllib_model.py")
+# y la direccioón del proyecto("{}/".format(PROJECT_HOME))
+
 # Train and persist the classifier model
 train_classifier_model_operator = BashOperator(
   task_id = "pyspark_train_classifier_model",
   bash_command = pyspark_bash_command,
   params = {
+  #Para asignar el master a un spark node que sea master se ha cambiado el campo "master": "local[8]" por:
     "master": "spark://master:7077",
     "filename": "resources/train_spark_mllib_model.py",
     "base_path": "{}".format(PROJECT_HOME)
